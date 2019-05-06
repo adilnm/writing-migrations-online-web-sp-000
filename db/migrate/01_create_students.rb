4b808fbbe96860c3ba871a4e6b7046dc20d3aee3
ADIL NAIMI <adilnm23@hotmail.com>
2019-05-06 05:19:38 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -6,3 +6,12 @@ class CreateStudents < ActiveRecord::Migration[5.1]
     end
   end
 end
+
+# class CreateStudents < ActiveRecord::Migration[5.1]
+# 
+#   def change
+#     add_column :students, :grade, :integer
+#     add_column :students, :birthdate, :integer
+#   end
+# end
+
